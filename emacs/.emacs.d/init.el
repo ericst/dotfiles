e08849dd37c09bffce215b4c08564219273dd34c
Eric Seuret <eric@ericst.ch>
2020-03-21 14:43:26 +0100

    Added git, modified emacs backup files

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,6 +24,14 @@
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 
+;;Backup files
+(setq backup-directory-alist `(("." . "~/emacs.d/backups")))
+(setq backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
 ;;Counsel, Ivy, Swiper
 (use-package counsel
   :ensure t
