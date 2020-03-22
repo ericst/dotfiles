43e5379f9eb9d51dae254624e630628238a14563
Eric Seuret <eric@ericst.ch>
2020-03-22 21:43:11 +0100

    Modified default theme to zenburn

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -5,8 +5,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" default)))
  '(delete-selection-mode nil)
- '(package-selected-packages (quote (general which-key counsel use-package))))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme general which-key counsel use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
