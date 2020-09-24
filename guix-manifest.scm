0cfc4e4d595dd24f8d6a469e7de9266f98d6c4cb
Eric Seuret <eric@ericst.ch>
2020-09-24 21:08:51 +0200

    Added guix manifest file

---
+++ guix-manifest.scm
@@ -0,0 +1,14 @@
+;;; guix-manifest.scm -- manifest of basic guix packages
+;; Meant to be installed with: ~guix package -m guix-manifest.scm~
+
+(specifications->manifest
+ '("stow"
+   "font-fira-code"
+   "font-fira-mono"
+   "font-fira-sans"
+   "guile"
+   "git"
+   "emacs"
+   "grep"
+   "ripgrep"
+   "the-silver-searcher"))
