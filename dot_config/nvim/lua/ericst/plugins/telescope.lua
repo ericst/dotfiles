468aa4696ec424b27b90f60944c902d7d8b3b86f
Eric Seuret <eric@ericst.ch>
2024-04-16 22:38:00 +0200

    Improved telescope configuration.

--- dot_config/nvim/lua/ericst/plugins/telescope.lua
+++ dot_config/nvim/lua/ericst/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
           vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
           vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
           vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+          vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
           vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {})
           vim.keymap.set('n', '<leader>fr', builtin.registers, {})
           vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
