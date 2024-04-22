fb8e7ac3d116ba4e33b4a59dd9418cf7c2bc0ac1
Eric Seuret <eric@ericst.ch>
2024-04-22 21:29:20 +0200

    Testing neogit.

--- dot_config/nvim/lua/ericst/plugins/diverse.lua
+++ dot_config/nvim/lua/ericst/plugins/diverse.lua
@@ -1,7 +1,4 @@
 -- Plugins which don't need lot of config for the moment...
-
 return {
-  {'tpope/vim-fugitive'}, -- For git usage
-  {'tpope/vim-sleuth'},   -- Heuristically sets tabs...
   {'jubnzv/IEC.vim'},   -- IEC language support
 }
