21b6ff106aec09e7c8ee94cfaad838544cc171e9
Eric Seuret <eric@ericst.ch>
2024-10-12 14:31:19 +0200

    Added surround

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -79,18 +79,18 @@ config.keys = {
   split_nav('resize', 'k'),
   split_nav('resize', 'l'),
 
-  {
-    key = 'Enter',
-    mods = 'LEADER',
-    action = wezterm.action.ActivateCopyMode
-  },
-
-  {
-    key = 'x',
-    mods = 'LEADER',
-    action = wezterm.action.ShowLauncher
-  },
-
+  { key = 'Enter', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+  { key = 'x', mods = 'LEADER', action = wezterm.action.ShowLauncher },
+  { key = 'F11', mods = 'NONE', action = wezterm.action.ToggleFullScreen, },
+  { key = '1', mods = 'LEADER', action = wezterm.action.ActivateTab(0) },
+  { key = '2', mods = 'LEADER', action = wezterm.action.ActivateTab(1) },
+  { key = '3', mods = 'LEADER', action = wezterm.action.ActivateTab(2) },
+  { key = '4', mods = 'LEADER', action = wezterm.action.ActivateTab(3) },
+  { key = '5', mods = 'LEADER', action = wezterm.action.ActivateTab(4) },
+  { key = '6', mods = 'LEADER', action = wezterm.action.ActivateTab(5) },
+  { key = '7', mods = 'LEADER', action = wezterm.action.ActivateTab(6) },
+  { key = '8', mods = 'LEADER', action = wezterm.action.ActivateTab(7) },
+  { key = '9', mods = 'LEADER', action = wezterm.action.ActivateTab(-1) },
 }
 
 return config
