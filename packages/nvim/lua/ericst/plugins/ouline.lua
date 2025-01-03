9798d379caba3cdc69e7be470f66bf361c417770
Eric Seuret <eric@ericst.ch>
2025-01-03 13:45:37 +0100

    Added outline.nvim.
    To keep track of keybindings, I also added a keybindings.md file.

---
+++ packages/nvim/lua/ericst/plugins/ouline.lua
@@ -0,0 +1,13 @@
+-- Default config:
+
+return {
+  "hedyhli/outline.nvim",
+  config = function()
+    -- Example mapping to toggle outline
+    vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+      { desc = "Toggle Outline" })
+
+    require("outline").setup {
+    }
+  end,
+}
