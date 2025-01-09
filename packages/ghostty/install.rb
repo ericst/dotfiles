a699ca26aa7c7b4815435d16b2db95265aed73b3
Eric Seuret <eric@ericst.ch>
2025-01-09 10:47:36 +0100

    Trying ghostty

--- packages/ghostty/install.rb
+++ packages/ghostty/install.rb
@@ -1,1 +1,3 @@
 link 'config', '~/.config/ghostty/config'
+link 'ghostty-workbox.desktop', '~/.local/share/applications/ghostty-workbox.desktop'
+link 'ghostty.desktop', '~/.local/share/applications/ghostty.desktop'
