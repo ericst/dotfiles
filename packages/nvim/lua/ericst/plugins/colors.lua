64b7c80fb937a50cd858d023433a7d5410ef484a
Eric Seuret <eric@ericst.ch>
2025-08-01 11:39:56 +0200

    Changed theme to tokyonight

--- packages/nvim/lua/ericst/plugins/colors.lua
+++ packages/nvim/lua/ericst/plugins/colors.lua
@@ -2,13 +2,11 @@
 
 return {
    {
-      'maxmx03/solarized.nvim',
+      'folke/tokyonight.nvim',
       lazy = false,
       priority = 1000,
       config = function()
-	vim.o.background = 'light' -- or 'dark'
-
-	vim.cmd.colorscheme 'solarized'
+	vim.cmd.colorscheme 'tokyonight-night'
       end,
     }, 
 }
