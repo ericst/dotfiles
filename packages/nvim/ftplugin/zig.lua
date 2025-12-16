b945bd44caea867a80329988f41b8aa935bb8ea3
Eric Seuret <eric@ericst.ch>
2025-12-16 06:31:24 +0100

    Improved Zig handling

--- packages/nvim/ftplugin/zig.lua
+++ packages/nvim/ftplugin/zig.lua
@@ -1,3 +1,5 @@
 -- Please don't forget to that this filetype is also part of the ctags updates
 
-vim.cmd [[ setlocal tags+=tags,~/Projects/zig/lib/std/tags ]] -- Somehow vim.opt_local doesntwork...
+-- Use ZIG_STD_DIR environment variable if available, fallback to hardcoded path
+local zig_std_tags = os.getenv("ZIG_STD_DIR") and (os.getenv("ZIG_STD_DIR") .. "/tags") or "~/Projects/zig/lib/std/tags"
+vim.cmd(string.format([[ setlocal tags+=tags,%s ]], zig_std_tags)) -- Somehow vim.opt_local doesntwork...
