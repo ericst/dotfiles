fcb2c47e5ed620505379299fcb2f813f1f0828eb
Eric Seuret <eric@ericst.ch>
2026-03-17 10:21:05 +0100

    Added Compose, improved git and ghostty config.

---
+++ packages/git/shell.sh
@@ -0,0 +1,14 @@
+# A few git aliases
+
+
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gps='git push'
+alias gpl='git pull'
+alias gf='git fetch'
+alias gm='git merge'
+alias gr='git rebase'
+alias go='git checkout'
+alias gl='git log --graph --oneline'
+
