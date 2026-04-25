9d36e3aed73537bab6c3682ad84e0712e9b9c3db
Eric Seuret <eric@ericst.ch>
2026-04-25 07:39:25 +0200

    chore(nvim): remove ctags integration
    Remove all ctags-related files and configuration from the Neovim setup,
    including the ctags generation module, Zig ctags config, Zig ftplugin,
    and ctags dependency from toolboxes. The ctags feature is being
    deprecated in favor of LSP-based navigation.

--- packages/nvim/ftplugin/zig.lua
+++
@@ -1,5 +0,0 @@
--- Please don't forget to that this filetype is also part of the ctags updates
-
--- Use ZIG_STD_DIR environment variable if available, fallback to hardcoded path
-local zig_std_tags = os.getenv("ZIG_STD_DIR") and (os.getenv("ZIG_STD_DIR") .. "/tags") or "~/Projects/zig/lib/std/tags"
-vim.cmd(string.format([[ setlocal tags+=tags,%s ]], zig_std_tags)) -- Somehow vim.opt_local doesntwork...
--- packages/nvim/lua/ericst/init.lua
+++ packages/nvim/lua/ericst/init.lua
@@ -2,7 +2,6 @@ require("ericst.options")
 require("ericst.lazy") -- TODO: Get rid of this to use vim.pack once every version I use is on 0.12...
 require("ericst.buffers")
 require("ericst.snippets")
-require("ericst.ctags")
 require("ericst.lsp")
 require("ericst.keymaps")
 
